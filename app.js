71fe3cfda1882f17748cc4a5b751aa8156e79c7b
Milu Farhan <milufarhan@gmail.com>
2023-09-04 14:57:23 +0530

    added error handling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -12,5 +14,11 @@ app.use(express.json());
 // 3) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,81 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  console.log(message);
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  console.log(err);
+
+  const message = `Duplicate field value:. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error("ERROR 💥", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      error: err,
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  console.log(process.env.NODE_ENV);
+
+  if (process.env.NODE_ENV === "development") {
+    console.log(err);
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+
+    console.log("Eqwwre:::", error.errors);
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error._message === "Validation failed") {
+      console.log("here its");
+      error = handleValidationErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
+  }
+};
